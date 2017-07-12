8063ea23547302d54516cea8adbde4c0a29b4c02
Xayaseth Boudsady <xboudsady@Seth-MacBook-Pro.local>
2017-07-11 22:39:22 -0700

    First commit

---
+++ script/script.js
@@ -0,0 +1,168 @@
+$(document).ready(function () {
+  $('#above-ground-tank').click(function () {
+    $('#above-ground-tank-attr').slideToggle("slow");
+  });
+  $('#air-compressor').click(function () {
+    $('#air-compressor-attr').slideToggle("slow");
+  });
+  $('#air-conditioning-unit').click(function () {
+    $('#air-conditioning-unit-attr').slideToggle("slow");
+  });
+  $('#air-dryer').click(function () {
+    $('#air-dryer-attr').slideToggle("slow");
+  });
+  $('#air-flow-control-terminal').click(function () {
+    $('#air-flow-control-terminal-attr').slideToggle("slow");
+  });
+  $('#air-handling-unit').click(function () {
+    $('#air-handling-unit-attr').slideToggle("slow");
+  });
+  $('#air-pressure-tank').click(function () {
+    $('#air-pressure-tank-attr').slideToggle("slow");
+  });
+  $('#air-separator').click(function () {
+    $('#air-separator-attr').slideToggle("slow");
+  });
+  $('#automatic-transfer-switch').click(function () {
+    $('#automatic-transfer-switch-attr').slideToggle("slow");
+  });
+  $('#backflow-prevention-device').click(function () {
+    $('#backflow-prevention-device-attr').slideToggle("slow");
+  });
+  $('#boiler-feedwater-pump').click(function () {
+    $('#boiler-feedwater-pump-attr').slideToggle("slow");
+  });
+  $('#chemical-injection-pump').click(function () {
+    $('#chemical-injection-pump-attr').slideToggle("slow");
+  });
+  $('#chill-water-pump').click(function () {
+    $('#chill-water-pump-attr').slideToggle("slow");
+  });
+  $('#chiller-air-cooled').click(function () {
+    $('#chiller-air-cooled-attr').slideToggle("slow");
+  });
+  $('#chiller-water-cooled').click(function () {
+    $('#chiller-water-cooled-attr').slideToggle("slow");
+  });
+  $('#chiller-absorption').click(function () {
+    $('#chiller-absorption-attr').slideToggle("slow");
+  });
+  $('#clocks-automated').click(function () {
+    $('#clocks-automated-attr').slideToggle("slow");
+  });
+  $('#condensate-return-pump').click(function () {
+    $('#condensate-return-pump-attr').slideToggle("slow");
+  });
+  $('#condensate-tank').click(function () {
+    $('#condensate-tank-attr').slideToggle("slow");
+  });
+  $('#condensate-drain-pump').click(function () {
+    $('#condensate-drain-pump-attr').slideToggle("slow");
+  });
+  $('#condenser-water-pump').click(function () {
+    $('#condenser-water-pump-attr').slideToggle("slow");
+  });
+  $('#condensing-unit').click(function () {
+    $('#condensing-unit-attr').slideToggle("slow");
+  });
+  $('#conductivity-controller').click(function () {
+    $('#conductivity-controller-attr').slideToggle("slow");
+  });
+  $('#control-air-compressor').click(function () {
+    $('#control-air-compressor-attr').slideToggle("slow");
+  });
+  $('#control-air-dryer').click(function () {
+    $('#control-air-dryer-attr').slideToggle("slow");
+  });
+  $('#control-damper').click(function () {
+    $('#control-damper-attr').slideToggle("slow");
+  });
+  $('#control-panel').click(function () {
+    $('#control-panel-attr').slideToggle("slow");
+  });
+  $('#cooling-coils').click(function () {
+    $('#cooling-coils-attr').slideToggle("slow");
+  });
+  $('#cooling-tower').click(function () {
+    $('#cooling-tower-attr').slideToggle("slow");
+  });
+  $('#cooling-tower-filter').click(function () {
+    $('#cooling-tower-filter-attr').slideToggle("slow");
+  });
+  $('#cooling-tower-filter-pump').click(function () {
+    $('#cooling-tower-filter-pump-attr').slideToggle("slow");
+  });
+  $('#day-tank').click(function () {
+    $('#day-tank-attr').slideToggle("slow");
+  });
+  $('#deaerator').click(function () {
+    $('#deaerator-attr').slideToggle("slow");
+  });
+  $('#deaerator-pump').click(function () {
+    $('#deaerator-pump-attr').slideToggle("slow");
+  });
+  $('#distilled-water-service-pump').click(function () {
+    $('#distilled-water-service-pump-attr').slideToggle("slow");
+  });
+  $('#domestic-cold-water-booster-system').click(function () {
+    $('#domestic-cold-water-booster-system-attr').slideToggle("slow");
+  });
+  $('#domestic-cold-water-pump').click(function () {
+    $('#domestic-cold-water-pump-attr').slideToggle("slow");
+  });
+  $('#domestic-hot-water-boiler').click(function () {
+    $('#domestic-hot-water-boiler-attr').slideToggle("slow");
+  });
+  $('#domestic-hot-water-heater').click(function () {
+    $('#domestic-hot-water-heater-attr').slideToggle("slow");
+  });
+  $('#domestic-hot-water-pump').click(function () {
+    $('#domestic-hot-water-pump-attr').slideToggle("slow");
+  });
+});
+
+
+
+// DEFAULT
+
+$(document).ready(function() {
+  $('#above-ground-storage-tank-default').click(function () {
+    $('#above-ground-storage-tank-default-show').slideToggle("slow");
+  });
+  $('#air-compressor-default').click(function () {
+    $('#air-compressor-default-show').slideToggle("slow");
+  });
+  $('#air-conditioning-unit-default').click(function () {
+    $('#air-conditioning-unit-default-show').slideToggle("slow");
+  });
+  $('#air-dryer-default').click(function () {
+    $('#air-dryer-default-show').slideToggle("slow");
+  });
+  $('#air-flow-control-terminal-default').click(function () {
+    $('#air-flow-control-terminal-default-show').slideToggle("slow");
+  });
+  $('#air-handling-unit-default').click(function () {
+    $('#air-handling-unit-default-show').slideToggle("slow");
+  });
+  $('#air-pressure-tank-default').click(function () {
+    $('#air-pressure-tank-default-show').slideToggle("slow");
+  });
+  $('#air-separator-default').click(function () {
+    $('#air-separator-default-show').slideToggle("slow");
+  });
+  $('#automatic-transfer-switch-default').click(function () {
+    $('#automatic-transfer-switch-default-show').slideToggle("slow");
+  });
+  $('#backflow-prevention-device-default').click(function () {
+    $('#backflow-prevention-device-default-show').slideToggle("slow");
+  });
+  $('#boiler-feedwater-pump-default').click(function () {
+    $('#boiler-feedwater-pump-default-show').slideToggle("slow");
+  });
+  $('#chemical-injection-pump-default').click(function () {
+    $('#chemical-injection-pump-default-show').slideToggle("slow");
+  });
+  $('#chill-water-pump-default').click(function () {
+    $('#chill-water-pump-default-show').slideToggle("slow");
+  });
+});
